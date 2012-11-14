944c1377fa0455d0b8cd4bf40a371fd50f3ff8c4
ville <ville@bitdeli.com>
2012-11-14 02:51:37 +0000

    fix incorrect time window for the chart

--- __main__.py
+++ __main__.py
@@ -22,7 +22,7 @@ def hourly_stats(profiles):
     for i, profile in enumerate(profiles):
         events = chain.from_iterable(profile['events'].itervalues())
         hours.update(frozenset(hour for hour, freq in events))
-    limit = max(hours) - CHART_HOURS * 24
+    limit = max(hours) - CHART_HOURS
     return [(datetime.utcfromtimestamp(hour * 3600).isoformat(), freq)
             for hour, freq in sorted(hours.iteritems()) if hour > limit]
 
